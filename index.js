32b18a65d455789abf1c18cd4ab0670faca34a0e
JojiAndela <42158522+JojiAndela@users.noreply.github.com>
2018-11-06 10:32:25 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Welcome to picsgram!' });
+});
+
+app.listen(port, () => {
+  console.log('App listening on port 3000!');
+});
